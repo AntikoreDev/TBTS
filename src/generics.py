fc67d61c53e97017a86b5eb265e3e326ae0acc96
Antikore <koopatropa8@hotmail.com>
2023-09-20 22:47:24 +0200

    Fixed emojis and added sentences

--- src/generics.py
+++ src/generics.py
@@ -1,4 +1,6 @@
-EMOJIS = list(set("🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐽🐸🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐞🐜🦗🕷️🦂🦟🦠🐢🐍🦎🐊🐅🐆🦓🦍🐘🦏🦛🦒🐫🐪🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓🥝🍅🥥🥑🍆🥔🥕🌽🌶️🥒🥬🥦🍄🥜🌰🍞🥐🥖🥨🥞🧀🍖🍗🥩🥓🍔🍟🍕🌭🍿🍲🍱🍘🍙🍚🍛🍜🍝🍠🍢🍣🍤🍞🍳🥓🍔🍟🍕🍣🍦🍪🍰🍫🍬🍭☕🍺🍷🥤🍽️🍴🧹🚽🚿🛁🪣🧻🧼🧽🔑🚪📱💻🖥️📷🎥⌚📻📺📚📰✉️📦📂🗂️📝🖊️📅📌📎✂️🧷🧵🧶🎮🧩🕯️📫📬📭📮📤📥🗳️✉️📧💌📩📪📠☎️📞📟📻📺🎙️🎚️🎛️🕰️⏰⏱️⏲️⏳⌛🧭🔋🔌💡🔦💸💰💳💎📿📲📱🎧🎤🎶🎵🎼🎻🎹🥁🎷🎺🎸🪕🪗🎭🖼️🎨"))
+VERSION = "1.0"
+
+EMOJIS = list(set("🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐽🐸🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐞🐜🦗️🦂🦟🦠🐢🐍🦎🐊🐅🐆🦓🦍🐘🦏🦛🦒🐫🐪🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓🥝🍅🥥🥑🍆🥔🥕🌽🥒🥬🥦🍄🥜🌰🍞🥐🥖🥨🥞🧀🍖🍗🥩🥓🍔🍟🍕🌭🍿🍲🍱🍘🍙🍚🍛🍜🍝🍠🍢🍣🍤🍞🍳🥓🍔🍟🍕🍣🍦🍪🍰🍫🍬🍭🍺🍷🥤🍽️🍴🧹🚽🚿🛁🧻🧼🧽🔑🚪📱💻️📷🎥📻📺📚📰️📦📂️📝🖊️📅📌📎🧷🧵🧶🎮🧩️📫📬📭📮️📧💌📩📪📠️📞📟📻📺️️️🧭🔋🔌💡🔦💸💰💳💎📿📲📱🎧🎤🎶🎵🎼🎻🎹🥁🎷🎺🎸🪕🎭️🎨"))
 ANSWER_LETTERS = list("ABCD")
-CORRECT_RESPONSES = ["That's right!", "So cool!", "Nice shot", "You're correct!"]
-INCORRECT_RESPONSES = ["No nitwit", "Bzzzz", "That's wrong", "Nope buddy"]
+CORRECT_RESPONSES = ["That's right!", "So cool!", "Nice shot", "You're correct!", "Good choice!"]
+INCORRECT_RESPONSES = ["No nitwit", "Bzzzz", "That's wrong", "Nope buddy", "F", "Pretty bad"]
