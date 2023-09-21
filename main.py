604823779624dfd4dd7daa7ab80e014a95814c3b
Antikore <koopatropa8@hotmail.com>
2023-09-21 14:55:57 +0200

    Moved game init here

---
+++ main.py
@@ -0,0 +1,17 @@
+from src.game import Game
+
+# This function defines the game object and initializes it
+def main():
+	game = Game() # Instantiate a game object
+	
+	try:
+		game.start()
+
+	# Whenever the user interrupts the terminal with Ctrl+C or similar, stop the game without raising an error
+	except KeyboardInterrupt:
+		game.stop()
+
+# Game Starter
+if (__name__ == "__main__"):
+	main()
+		
