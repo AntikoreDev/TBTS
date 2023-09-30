b8b2aab464f11c6a54f3085c58a54ea1fd018833
Antikore <koopatropa8@hotmail.com>
2023-09-30 22:00:26 +0200

    ♻️ Refactorized variables

--- src/question.py
+++ src/question.py
@@ -3,13 +3,14 @@ import random
 
 # Class that holds data for a particular question
 class Question():
-	question = ""
-	answers = []
-	correct = ""
-	category = ""
-	difficulty = ""
+	question: str = "" # The question text
+	answers: list = [] # The list of answers
 
-	def __init__(self, question = "", answers = [], category = "", difficulty = ""):
+	correct: str = "" # The correct answer as an string
+	category: str = "" # The category of the question 
+	difficulty: str = "" # The difficulty of the question
+
+	def __init__(self, question: str = "", answers: list = [], category: str = "", difficulty: str = ""):
 		self.question = self.parse_html(question)
 		self.category = self.parse_html(category)
 		self.difficulty = self.parse_html(difficulty)
@@ -19,7 +20,8 @@ class Question():
 		
 		random.shuffle(self.answers)
 
-	def parse_html(self, data = "") -> any:
+	# Parse the HTMLified string
+	def parse_html(self, data: str = "") -> str | list:
 
 		if (isinstance(data, list)):
 			new_data = []
