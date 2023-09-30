0041434af305bbebecda24a150abd7561c1cf202
Antikore <koopatropa8@hotmail.com>
2023-09-30 21:57:08 +0200

    🎨 Improved constants

--- src/commons.py
+++ src/commons.py
@@ -1,7 +1,13 @@
+"""
+This python function contains various constants used along the game
+"""
 VERSION = "1.0"
 GITHUB_REPO = "https://github.com/AntikoreDev/TBTS"
 
 EMOJIS = list(set("🐶🐱🐭🐹🐰🦊🐻🐼🐨🐯🦁🐮🐷🐽🐸🐒🐔🐧🐦🐤🐣🐥🦆🦅🦉🦇🐺🐗🐴🦄🐝🐛🦋🐌🐞🐜🦗️🦂🦟🦠🐢🐍🦎🐊🐅🐆🦓🦍🐘🦏🦛🦒🐫🐪🍇🍈🍉🍊🍋🍌🍍🍎🍏🍐🍑🍒🍓🥝🍅🥥🥑🍆🥔🥕🌽🥒🥬🥦🍄🥜🌰🍞🥐🥖🥨🥞🧀🍖🍗🥩🥓🍔🍟🍕🌭🍿🍲🍱🍘🍙🍚🍛🍜🍝🍠🍢🍣🍤🍞🍳🥓🍔🍟🍕🍣🍦🍪🍰🍫🍬🍭🍺🍷🥤🍽️🍴🧹🚽🚿🛁🧻🧼🧽🔑🚪📱💻️📷🎥📻📺📚📰️📦📂️📝🖊️📅📌📎🧷🧵🧶🎮🧩️📫📬📭📮️📧💌📩📪📠️📞📟📻📺️️️🧭🔋🔌💡🔦💸💰💳💎📿📲📱🎧🎤🎶🎵🎼🎻🎹🥁🎷🎺🎸🪕🎭️🎨"))
 ANSWER_LETTERS = list("ABCD")
-CORRECT_RESPONSES = ["That's right!", "So cool!", "Nice shot", "You're correct!", "Good choice!"]
-INCORRECT_RESPONSES = ["No nitwit", "Bzzzz", "That's wrong", "Nope buddy", "F", "Pretty bad"]
+CORRECT_RESPONSES = ["That's right!", "So cool!", "Nice shot", "You're correct!", "Good choice!", "Wise option :)", "True!"]
+INCORRECT_RESPONSES = ["No nitwit", "Bzzzz", "That's wrong", "Nope buddy", "F", "Pretty bad", "That feels sad", "Incorrect!", "False"]
+
+TRIVIA_LETTERS = list("ABCD")
+ROUND_QUESTION_COUNT = 15
